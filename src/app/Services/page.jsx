b05507e9d0b6daff0d38971871b84b657d8ff200
Komal <ks989156@gmail.com>
2025-11-27 16:38:01 +0530

    Add new images and update Services page layout
    - Added new images: question.svg, sleeping.jpg, and whyUs.jpg.
    - Updated Services page layout with improved styling and structure.
    - Enhanced background image and overlay for better visual appeal.
    - Adjusted text content and formatting for clarity and emphasis.
    - Introduced new sections highlighting expertise, quality products, and personalized support.

--- src/app/Services/page.jsx
+++ src/app/Services/page.jsx
@@ -7,17 +7,23 @@ import Image from "next/image";
 export default function Services() {
   return (
     <div className="flex flex-col items-center justify-centerspace-y-16">
-      <section className="border-1 relative flex justify-between items-center h-[400px] w-[80%]">
+      <section className="relative flex justify-between items-center h-[390px] w-[90%] rounded-l-2xl ">
+        {/* Background Image */}
         <Image
           src="/sleeping.jpg"
           alt="sleeping"
-          width={950}
-          height={900}
-          className="object-cover"
-        ></Image>
-        <div className="absolute z-20">
+          width={1100}
+          height={1000}
+          className="object-cover w-full h-full rounded-l-2xl"
+        />
+
+        {/* Black Transparent Overlay */}
+        <div className="absolute inset-0 bg-gradient-to-r from-[#0077b6]/40 to-transparent z-10 rounded-l-2xl"></div>
+
+        {/* Text Content */}
+        <div className="absolute z-20 px-8">
           <div className="flex flex-col gap-4 text-white">
-            <h2 className="text-4xl font-medium">Our Services</h2>
+            <h2 className="text-6xl font-semibold">Our Services</h2>
             <p className="text-base font-medium max-w-3/5">
               At our organisation, we are committed to delivering high-quality
               respiratory care solutions that support better breathing, improved
@@ -25,7 +31,6 @@ export default function Services() {
             </p>
           </div>
         </div>
-        
       </section>
 
       <section className="flex flex-col h-auto items-center text-center gap-4 px-5">
@@ -34,33 +39,36 @@ export default function Services() {
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5 }}
-            className=" p-4 m-4 text-left"
+            className="bg-[#0077b6]/40 p-4 m-4 text-left rounded-xl w-1/3"
           >
             <div className="rounded-full w-13 h-13 flex-shrink-0 flex items-center justify-center bg-[#F5F5F5] m-4">
-              <Image 
-                src="https://www.reshot.com/preview-assets/icons/7SJT2D5QLV/oxygen-mask-7SJT2D5QLV.svg" 
-                alt="icon" 
-                width={30} 
+              <Image
+                src="https://www.reshot.com/preview-assets/icons/7SJT2D5QLV/oxygen-mask-7SJT2D5QLV.svg"
+                alt="icon"
+                width={30}
                 height={30}
               />
             </div>
             <h2 className="mb-2 text-2xl font-medium px-5 py-2">
               Medical Device Sales
             </h2>
-            <p className="text-base font-light px-5 py-2 ">Choose high-quality Oxygen Concentrators andCPAP/BiPAP range sourced from trusted manufacturers.</p>
+            <p className="text-base font-light px-5 py-2 ">
+              Choose high-quality Oxygen Concentrators andCPAP/BiPAP range
+              sourced from trusted manufacturers.
+            </p>
           </motion.div>
 
           <motion.div
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 0.5 }}
-            className=" p-4 m-4 text-left"
+            className="bg-[#0077b6]/40 p-4 m-4 text-left rounded-xl w-1/3"
           >
             <div className="rounded-full w-13 h-13 flex-shrink-0 flex items-center justify-center bg-[#F5F5F5] m-4">
-              <Image 
-                src="https://www.reshot.com/preview-assets/icons/X5YAMKP9BE/rental-X5YAMKP9BE.svg" 
-                alt="icon" 
-                width={30} 
+              <Image
+                src="https://www.reshot.com/preview-assets/icons/X5YAMKP9BE/rental-X5YAMKP9BE.svg"
+                alt="icon"
+                width={30}
                 height={30}
               />
             </div>
@@ -68,7 +76,8 @@ export default function Services() {
               Equipment Rental
             </h2>
             <p className="text-base font-light px-5 py-2">
-              Get access to the medical equipment you need—without long-term commitments.
+              Get access to the medical equipment you need—without long-term
+              commitments.
             </p>
           </motion.div>
 
@@ -76,13 +85,13 @@ export default function Services() {
             initial={{ y: 100, opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.5, delay: 1 }}
-            className=" p-4 m-4 text-left"
+            className="bg-[#0077b6]/40 p-4 m-4 text-left rounded-xl w-1/3"
           >
             <div className="rounded-full w-13 h-13 flex-shrink-0 flex items-center justify-center bg-[#F5F5F5] m-4">
-              <Image 
-                src="https://www.reshot.com/preview-assets/icons/7SLFANCXGJ/sleep-7SLFANCXGJ.svg" 
-                alt="icon" 
-                width={30} 
+              <Image
+                src="https://www.reshot.com/preview-assets/icons/7SLFANCXGJ/sleep-7SLFANCXGJ.svg"
+                alt="icon"
+                width={30}
                 height={30}
               />
             </div>
@@ -90,85 +99,44 @@ export default function Services() {
               Sleep Diagnostics
             </h2>
             <p className="text-base font-light px-5 py-2">
-              Our experts conducts comprehensive sleep studies for better sleep health.
+              Our experts conducts comprehensive sleep studies for better sleep
+              health.
             </p>
           </motion.div>
         </div>
       </section>
 
-      <div className="bg-[#0077b6] text-white flex flex-col h-[500px] items-center text-center gap-6">
-        <div className="flex flex-col gap-6 mt-6">
-          <h2 className="text-4xl font-medium">Types of Sleep Studies</h2>
-          <p className="text-lg font-medium text-gray-100">
-            We bridge the gap between learning and employability by helping
-            students build portfolio-worthy projects that recruiters love.
-          </p>
+      <div id='WhyUs' className=' md:mt-15 lg:mt-6'>
+
+      <h4 className='text-xl lg:text-4xl font-medium max-w-full md:text-2xl text-center'>Why Choose SNK Global Consultancy Services? </h4>
+      
+      <div className='lg:grid md:grid lg:grid-cols-2 md:grid-cols-2 lg:h-4/5 py-15 px-10'> 
+        <div className='w-full h-full flex justify-center items-center'>
+          <Image src="/whyUs.jpg" alt="question-mark" height={500} width={500}></Image>
         </div>
 
-        <div className="flex flex-col lg:flex-row gap-4 p-4 lg:mt-5">
-          <motion.div
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5 }}
-            className=" p-4 m-4 text-left"
-          >
-            <h2 className="mb-2 text-2xl font-medium px-5 py-2">
-              Polysomnography or Diagnostic Tests
-            </h2>
-            <p className="text-base font-light px-5 py-2 ">
-              Once diagnosed with a breathing disorder, CPAP or BiPAP titration
-              tests are done to determine which mode and what pressure settings
-              to set for your therapy.
-            </p>
-          </motion.div>
+        <div className='flex flex-col gap-3 lg:gap-12 justify-center px-4 md:gap-4' >
 
-          <motion.div
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
-            className=" p-4 m-4 text-left"
-          >
-            <h2 className="mb-2 text-2xl font-medium px-5 py-2">
-              PAP Titration Tests
-            </h2>
-            <p className="text-base font-light px-5 py-2">
-              Each project is scoped to demonstrate technical depth while
-              staying short enough to complete and deploy quickly.
-            </p>
-          </motion.div>
+          <div className=' bg-black text-white h-auto p-4 shadow-gray-600 shadow-lg'>
+            <h3 className='text-sm lg:text-xl font-extrabold px-2 md:text-sm'>Expertise</h3>
+            <p className='text-xs lg:text-base font-extralight px-5 py-2 md:text-xs'> Our team has extensive experience in the respiratory care industry, ensuring that our clients receive the best possible service.</p>
+          </div >
 
-          <motion.div
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.5, delay: 1 }}
-            className=" p-4 m-4 text-left"
-          >
-            <h2 className="mb-2 text-2xl font-medium px-5 py-2">
-              Split Night Tests
-            </h2>
-            <p className="text-base font-light px-5 py-2">
-              A mix of diagnostic and PAP titration test. Saves cost by
-              combining both in a single night.
-            </p>
-          </motion.div>
+          <div className=' bg-black text-white h-auto p-4 ml-10 shadow-gray-600 shadow-md'>
+            <h3 className='text-sm lg:text-xl font-extrabold px-2 md:text-sm'>Quality Products</h3>
+            <p className='text-xs lg:text-base font-extralight px-5 py-2 md:text-xs'> We offer high-quality products from leading manufacturers, ensuring that our clients receive the best possible outcomes.</p>
+          </div>
+
+          <div className=' bg-black text-white h-auto p-4 shadow-gray-600 shadow-md'>
+            <h3 className='text-sm lg:text-xl font-extrabold px-2 md:text-sm'>Personalized Support</h3>
+            <p className='text-xs lg:text-base font-extralight px-5 py-2 md:text-xs'> We prioritize building strong relationships with our clients, providing personalized support and service to meet their unique needs.</p>
+          </div>
+
         </div>
+
       </div>
 
-      {/* Diagnostic Equipment / Partners */}
-      <section className="max-w-4xl mx-auto space-y-4">
-        <h2 className="text-2xl font-medium">Diagnostic Equipment Partners</h2>
-        <p className="text-gray-700">
-          We use advanced testing equipment from world-class medical technology
-          partners.
-        </p>
-      </section>
-
-      {/* Call to Action */}
-      <div className="max-w-3xl mx-auto text-center pt-12">
-        <button className="bg-[#0077b6] text-white px-6 py-3 rounded-lg hover:bg-[#005f87] transition">
-          Book Your Sleep Study
-        </button>
-      </div>
+    </div>
     </div>
   );
 }
